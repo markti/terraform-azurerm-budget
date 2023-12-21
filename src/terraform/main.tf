75973fb5790451a5c96f62aa281340a1ca9a828d
markti <markti@outlook.com>
2023-12-21 10:28:50 -0600

    add budget resource

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -4,3 +4,30 @@ resource "azurerm_resource_group" "main" {
 }
 
 data "azurerm_client_config" "current" {}
+
+resource "time_static" "budget_start_date" {
+}
+
+resource "azurerm_consumption_budget_subscription" "main" {
+
+  name            = "budget-${var.application_name}-${var.environment_name}"
+  subscription_id = data.azurerm_client_config.current.subscription_id
+
+  amount     = 50
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = time_static.budget_start_date.rfc3339
+  }
+
+  notification {
+    enabled        = true
+    threshold      = 90.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "mark@tinderholt.net"
+    ]
+  }
+}
